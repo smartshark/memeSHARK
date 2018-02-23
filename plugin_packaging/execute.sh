b580d135f7dcaa458549743dcc3b5c259220ccb2
sherbold <herbold@cs.uni-goettingen.de>
2018-02-23 08:46:20 +0100

    Fixed parameter information

--- plugin_packaging/execute.sh
+++ plugin_packaging/execute.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 PLUGIN_PATH=$1
 
-COMMAND="python3.5 $1/main.py --db-database $4 --db-hostname $5 --db-port $6"
-
-if [ ! -z ${2} ] && [ ${2} != "None" ]; then
-	COMMAND="$COMMAND --db-user ${2}"
-fi
+COMMAND="python3.5 $1/main.py --project-name $2 --db-database $5 --db-hostname $6 --db-port $7"
 
 if [ ! -z ${3} ] && [ ${3} != "None" ]; then
-	COMMAND="$COMMAND --db-password ${3}"
+	COMMAND="$COMMAND --db-user ${3}"
 fi
 
-if [ ! -z ${7} ] && [ ${7} != "None" ]; then
-	COMMAND="$COMMAND --db-authentication ${7}"
+if [ ! -z ${4} ] && [ ${4} != "None" ]; then
+	COMMAND="$COMMAND --db-password ${4}"
 fi
 
 if [ ! -z ${8} ] && [ ${8} != "None" ]; then
-    COMMAND="$COMMAND --debug ${8}"
+	COMMAND="$COMMAND --db-authentication ${8}"
 fi
 
 if [ ! -z ${9} ] && [ ${9} != "None" ]; then
+    COMMAND="$COMMAND --debug ${8}"
+fi
+
+if [ ! -z ${10} ] && [ ${10} != "None" ]; then
     COMMAND="$COMMAND --ssl"
 fi
 
